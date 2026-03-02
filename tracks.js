70f11e5eb1919b200e7f035c230fa85645263223
kaikaiphoto <168066601+kaikaiphoto@users.noreply.github.com>
2026-03-02 11:10:55 +0800

    Create tracks.js

---
+++ tracks.js
@@ -0,0 +1,18 @@
+const tracks = [
+  {
+    slug: "jcws",           // 子域名
+    title: "第01首 - 轻快夏日流行",
+    cover: "https://audio.1804078.xyz/covers/01.jpg",
+    audioMP3: "https://audio.1804078.xyz/tracks/01.mp3",
+    audioWAV: "https://audio.1804078.xyz/tracks/01.wav"
+  },
+  // 第2首开始复制下面这块，改数字和名字即可
+  {
+    slug: "track002",
+    title: "第02首 - 电子舞曲风",
+    cover: "https://audio.1804078.xyz/covers/02.jpg",
+    audioMP3: "https://audio.1804078.xyz/tracks/02.mp3",
+    audioWAV: "https://audio.1804078.xyz/tracks/02.wav"
+  }
+  // ... 你可以先只放1条，后面每天加
+];
